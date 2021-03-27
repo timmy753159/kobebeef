ab727594218c6884590e953e24c06206e9f41e83
timmy753159 <109021491@live.asia.edu.tw>
2021-03-27 09:58:38 +0800

    add python

--- Min-Shiang_Hwang.py
+++ Min-Shiang_Hwang.py
@@ -5,15 +5,15 @@ from bs4 import BeautifulSoup
 req = req.get("http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
 req.encoding = "utf8"
 if req.status_code == 200:
-    soup = BeautifulSoup(req.text, "lxml")
-    result1 = soup.find_all("li")
 
-    fp = open("outHwang.text","w",encoding="big5")
+    soup = BeautifulSoup(req.text, "lxml")
+    result1 = soup.find_all("span",lang='EN-US')
+
+    fp = open("outHwang.text","w",encoding="utf8")
     for val in result1:
         text2 = val.text.replace('\n', '')
-        text3 = text2.replace('   ', '')
-        print(text3)
-        fp.write(text3+"\n")
+        print(text2)
+        fp.write(text2+"\n")
     fp.close()
 else:
     print("no page")
--- Wong.py
+++ Wong.py
@@ -3,16 +3,16 @@ from bs4 import BeautifulSoup
 
 
 req = req.get("http://dns2.asia.edu.tw/~jdwang/PaperList.htm")
-req.encoding = "utf8"
+req.encoding = "big5"
 if req.status_code == 200:
     soup = BeautifulSoup(req.text, "lxml")
     result1 = soup.find_all("li")
-    fp = open("outWong.text","w",encoding="utf8")
+    fp = open("outWong.text","w",encoding="big5")
     for val in result1:
-        text2 = val.text.replace('\n', '')
-        text3 = text2.replace('   ', '')
-        print(text3)
-        fp.write(text3+"\n")
+        text2 = val.text.replace('\xa0', 'U')
+        text2 = text2.replace('\n', '')
+        print(text2)
+        fp.write(text2+"\n")
     fp.close()
 else:
     print("no page")
